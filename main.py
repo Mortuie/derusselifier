a399c25351fb2ee4cfcddabfd932a8da08d44a2c
mortuie <jimmybob-leon@hotmail.co.uk>
2019-10-21 15:31:05 +0200

    Working prototype of program

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
-import sys, os
+import os
 
-def merge(files, destination):
-    merged = PdfFileMerger()
+def merge_files(destination_path, files):
+    merged_file = PdfFileMerger()
 
     for file in files:
         with open(file, 'rb') as f:
@@ -11,44 +11,25 @@ def merge(files, destination):
             if pdf.isEncrypted:
                 pdf.decrypt('')
 
-            merged.append(pdf)
+            merged_file.append(pdf)
 
-    with open(destination, 'wb') as output:
-        merged.write(output)
+    with open(destination_path, 'wb') as output:
+        merged_file.write(output)
+    return True
 
-def merge_multiple_files(files, destination):
-    unique_pdf = PdfFileWriter()
-
-    for file in files:
-        print("Trying to add this file: " + file + "!")
-        with open(file, 'rb') as f:
-            pdf = PdfFileReader(f)
-            if pdf.isEncrypted:
-                pdf.decrypt('')
-            number_of_pages = pdf.getNumPages()
-
-            for page in range(0, number_of_pages):
-                pdfpage = pdf.getPage(page)
-                print(pdfpage.extractText())
-                unique_pdf.addPage(pdfpage)
-
-    for page in range(unique_pdf.getNumPages()):
-        print(unique_pdf.getPage(page).extractText())
-    return unique_pdf
-
-def remove_slides(source, destination):
+def remove_slides(source_path, destination_path):
     number_of_pages = 0
     unique_pages = []
 
     unique_pdf = PdfFileWriter()
 
-    with open(source, 'rb') as f:
+    with open(source_path, 'rb') as f:
         pdf = PdfFileReader(f)
         if pdf.isEncrypted:
             pdf.decrypt('')
         number_of_pages = pdf.getNumPages()
 
-        for pages in range(0, number_of_pages):
+        for pages in range(number_of_pages):
             curr_page = ''.join(pdf.getPage(pages).extractText().split()[:-1])
             if pages == number_of_pages - 1:
                 unique_pdf.addPage(pdf.getPage(pages))
@@ -61,17 +42,12 @@ def remove_slides(source, destination):
                 unique_pdf.addPage(pdf.getPage(pages))
 
 
-        with open(destination, "wb") as output:
+        with open(destination_path, "wb") as output:
             unique_pdf.write(output)
 
 if __name__ == '__main__':
-    z = list(filter(lambda x: x.startswith("slides"), os.listdir()))
-    z.sort()
-    print(z)
-    merge(z, 'test_merge.pdf')
-    # try:
-    #     source, destination = sys.argv[1], sys.argv[2]
-    # except:
-    #     print("Not enough cli args passed in. Need at least 2 for source and destination paths.")
-    #     exit(1)
-    # remove_slides(source, destination)
+    list_of_pdfs = list(filter(lambda x: x.startswith("slides"), os.listdir()))
+    list_of_pdfs.sort()
+    if merge_files('merged.pdf', list_of_pdfs):
+        remove_slides('merged.pdf', 'small.pdf')
+        print("We are done!")
